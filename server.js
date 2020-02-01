2de942f59b001efe5c18b9cc6e8d1903cbe97e25
pb-totoro <jonesec2@gmail.com>
2020-01-31 22:21:03 -0500

    Updated post and delete

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs")
 const db = require("./db/db.json")
+const Sugar = require("sugar")
 
-// console.log(db)
+Sugar.extend();
 
 //=================================================
 const app = express();
@@ -33,24 +34,38 @@ app.get("/", function (req, res) {
 // API routes
 //=================================================
 app.get("/api/notes", function (req, res) {
-    // console.log(res.json(db))
+
     return res.json(db);
 });
 
 app.post("/api/notes", function (req, res) {
     let newNote = req.body;
-
+    newNote['id'] = db.length + 1
+    console.log(newNote)
     db.push(newNote);
 
-    fs.writeFile('./db/db.json', JSON.stringify(db), function(err) {
+    fs.writeFile('./db/db.json', JSON.stringify(db), function (err) {
         if (err) throw err;
         console.log("Note added!")
     })
 
 });
 
+
 app.delete("/api/notes/:id", function (req, res) {
     
+    const chosenNote = req.params.id;
+    console.log(db[1].id)
+    console.log(chosenNote)
+    // let newArray = db.remove(function(el) {return el.id === chosenNote})
+    // console.log(JSON.stringify(newArray))
+
+    // db.push(newArray);
+
+    fs.writeFile('./db/db.json', JSON.stringify(db), function (err) {
+        if (err) throw err;
+        "Note Deleted"
+    })
 })
 
 
